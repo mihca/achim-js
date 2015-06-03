b9f2330a15295b7f7f695476cc1084ab6e4194a1
muellera <achim.mueller@msg-systems.com>
2015-06-03 12:44:39 +0200

    Code refactored

--- main.js
+++ main.js
@@ -4,14 +4,20 @@ import StravaSeason from "./season"
 
     String.prototype.toHHMMSS = function () {
         var sec_num = parseInt(this, 10); // don't forget the second param
-        var hours   = Math.floor(sec_num / 3600);
+        var hours = Math.floor(sec_num / 3600);
         var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
         var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-        if (hours   < 10) {hours   = "0"+hours;}
-        if (minutes < 10) {minutes = "0"+minutes;}
-        if (seconds < 10) {seconds = "0"+seconds;}
-        var time    = hours+':'+minutes;
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        var time = hours + ':' + minutes;
         return time;
     }
 
@@ -22,30 +28,55 @@ import StravaSeason from "./season"
         $.markup.debug = 4;
         $.markup.load(function () {
 
-            var stravaSeason = new StravaSeason();
+            let stravaSeason2013 = new StravaSeason(2013);
+            let stravaSeason2014 = new StravaSeason(2014);
+            let stravaSeason2015 = new StravaSeason(2015);
 
-            let fetchPromised = (year, details) => {
+            var fetchPromised = (stravaSeason) => {
                 return new Promise((resolve, reject) => {
-                    stravaSeason.calYear(year, details, resolve, reject)
+                    stravaSeason.fetchData(resolve, reject)
                 })
             };
 
             Promise.all([
-                fetchPromised(2013, true),
-                fetchPromised(2014, true),
-                fetchPromised(2015, true)
-            ]).then(([ sum2013, sum2014, sum2015 ]) => {
-                // let [ sum2013, sum2014, sum2015 ] = data
+                fetchPromised(stravaSeason2013),
+                fetchPromised(stravaSeason2014),
+                fetchPromised(stravaSeason2015)
+            ]).then(([ data2013, data2014, data2015 ]) => {
+
+                let sum2013 = stravaSeason2013.sumAll();
+                let sum2014 = stravaSeason2014.sumAll();
+                let sum2015 = stravaSeason2015.sumAll();
+
+                let chart_data = [
+                    ["Year", "km Swim", "km Bike", "km Run"],
+                    ["2013", sum2013["Swim"], sum2013["Ride"], sum2013["Run"]],
+                    ["2014", sum2014["Swim"], sum2014["Ride"], sum2014["Run"]],
+                    ["2015", sum2015["Swim"], sum2015["Ride"], sum2015["Run"]]
+                ]
+
+                let chart_data2 = [
+                    ["Discipline", "2013", "2014", "2015"],
+                    ["Swim", sum2013["Swim"], sum2014["Swim"], sum2015["Swim"]],
+                    ["Ride", sum2013["Ride"], sum2014["Ride"], sum2015["Ride"]],
+                    ["Run", sum2013["Run"], sum2014["Run"], sum2015["Run"]]
+                ]
+
                 $("body").markup("chart", {
                     title: "Disciplines",
                     chart_type: 'line',
-                    chart_data: '[["Year", "km Swim", "km Bike", "km Run"], ["2013", '+sum2013['Swim']+', '+sum2013['Ride']+','+sum2013['Run']+'], ["2014", '+sum2014['Swim']+', '+sum2014['Ride']+','+sum2014['Run']+'], ["2015", '+sum2015['Swim']+', '+sum2015['Ride']+','+sum2015['Run']+']]'
+                    chart_data: JSON.stringify(chart_data)
                 });
                 $("body").markup("chart", {
                     title: "Disciplines",
                     chart_type: 'bar',
-                    chart_data: '[["Year", "km Swim", "km Bike", "km Run"], ["2013", '+sum2013['Swim']+', '+sum2013['Ride']+','+sum2013['Run']+'], ["2014", '+sum2014['Swim']+', '+sum2014['Ride']+','+sum2014['Run']+'], ["2015", '+sum2015['Swim']+', '+sum2015['Ride']+','+sum2015['Run']+']]'
+                    chart_data: JSON.stringify(chart_data2)
                 });
+
+                stravaSeason2013.showTable();
+                stravaSeason2014.showTable();
+                stravaSeason2015.showTable();
+
             }, (err) => {
                 console.log('error: ${err}')
             })
--- season.js
+++ season.js
@@ -1,84 +1,75 @@
 export default class StravaSeason {
 
-  constructor(debug = false){
-      this.name = 'Season Wrapper';
-  }
+    constructor(saison) {
+        this.data = [];
+        this.saison = saison;
+    }
 
-  calYear (saison, showTable, resolve, reject) {
+    fetchData(resolve, reject) {
 
-      $.ajax({
-          url: "https://www.strava.com/api/v3/activities?access_token=1a116d53dfe037cedbc5b9c19d173d38afc8435a&per_page=200&after=" + new Date((saison - 1) + '-11-20').getTime() / 1000,
-          dataType: "jsonp"
-      }).then(function (data) {
+        var self = this;
 
-          var sum = {};
-          var notBefore = new Date((saison - 1) + '-11-20');
-          var notAfter = new Date(saison + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate());
-          // var notAfter = new Date(saison + '-07-13');
+        $.ajax({
+            url: "https://www.strava.com/api/v3/activities?access_token=1a116d53dfe037cedbc5b9c19d173d38afc8435a&per_page=200&after=" + new Date((this.saison - 1) + '-11-20').getTime() / 1000,
+            dataType: "jsonp"
+        }).then(function (data) {
 
-          $("body").markup("table", {
-              range_from: notBefore.toLocaleDateString(),
-              range_to: notAfter.toLocaleDateString()
-          })
+            self.from = new Date((self.saison - 1) + '-11-20');
+            self.to = new Date(self.saison + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate());
+            let j = 0;
 
-          for (var i = 0; i < data.length; i++) {
+            for (let i = 0; i < data.length; i++) {
 
-              var activityDate = new Date(data[i].start_date_local.substring(0, 10));
-              if (activityDate < notAfter) {
-                  if (showTable) {
-                      $("tbody").markup("table/row", {
-                          k: i%2,
-                          i: i+1,
-                          title: data[i].name,
-                          date: data[i].start_date_local.substring(0, 10),
-                          type: data[i].type,
-                          time: data[i].moving_time.toString().toHHMMSS(),
-                          distance: $.number(data[i].distance / 1000, 1, ',', ''),
-                          link: "https://www.strava.com/activities/"+data[i].id
-                      })
-                  }
-                  if (isNaN(sum[data[i].type]))
-                      sum[data[i].type] = data[i].distance / 1000
-                  else
-                      sum[data[i].type] = sum[data[i].type] + data[i].distance / 1000;
-              }
-          }
+                let activityDate = new Date(data[i].start_date_local.substring(0, 10));
+                if (activityDate < self.to) self.data[j++] = data[i];
+            }
 
-          $("tbody").markup("table/row", {
-              i: saison,
-              k: 0,
-              title: 'Gesamt',
-              date: '',
-              time: '',
-              type: 'Swim',
-              distance: $.number(sum['Swim'], 1, ',', '')
-          })
-          $("tbody").markup("table/row", {
-              i: saison,
-              k: 0,
-              title: 'Gesamt',
-              date: '',
-              time: '',
-              type: 'Ride',
-              distance: $.number(sum['Ride'], 1, ',', '')
-          })
-          $("tbody").markup("table/row", {
-              i: saison,
-              k: 0,
-              title: 'Gesamt',
-              date: '',
-              time: '',
-              type: 'Run',
-              distance: $.number(sum['Run'], 1, ',', '')
-          })
+            resolve(self.data);
+        });
+    }
 
-          for (var property in sum) {
-              if (sum.hasOwnProperty(property)) {
-                  sum[property] = Math.round(sum[property]*10)/10;
-              }
-          }
+    sumAll() {
 
-          resolve (sum);
-      });
-  }
+        var sum = {};
+
+        for (let i = 0; i < this.data.length; i++) {
+
+            if (isNaN(sum[this.data[i].type]))
+                sum[this.data[i].type] = this.data[i].distance / 1000
+            else
+                sum[this.data[i].type] = sum[this.data[i].type] + this.data[i].distance / 1000;
+        }
+
+        for (var property in sum) {
+            if (sum.hasOwnProperty(property)) {
+                sum[property] = Math.round(sum[property]*10)/10;
+            }
+        }
+
+        return (sum);
+    }
+
+    showTable() {
+
+        let h = $.markup("table", {
+            range_from: this.from.toLocaleDateString(),
+            range_to: this.to.toLocaleDateString()
+        })
+
+        for (let i = 0; i < this.data.length; i++) {
+
+            $("tbody", h).markup("table/row", {
+                k: i%2,
+                i: i+1,
+                title: this.data[i].name,
+                date: this.data[i].start_date_local.substring(0, 10),
+                type: this.data[i].type,
+                time: this.data[i].moving_time.toString().toHHMMSS(),
+                distance: $.number(this.data[i].distance / 1000, 1, ',', ''),
+                link: "https://www.strava.com/activities/"+this.data[i].id
+            })
+        }
+
+        $("body").append(h);
+    }
 }
